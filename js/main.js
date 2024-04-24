084c32f4664a4a82ae986b7838205c3ed03dfa0d
spacecreator8 <spcc888@ya.ru>
2024-04-24 10:45:06 +0700

    5.Рендеринг списков

--- js/main.js
+++ js/main.js
@@ -5,8 +5,21 @@ let app = new Vue({
         image: "./assets/vmSocks-green-onWhite.jpg",
         altText: "A pair of socks",
         inStock: true,
-        inventory: 100,
-        onSale: true,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green'
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue'
+            }
+         ],
+         sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+
+         
     }
  })
+ 
  
