45e801ccaa9dfdfefbb659e39269917081bce941
spacecreator8 <spcc888@ya.ru>
2024-04-24 10:23:31 +0700

    4. Условный рендеринг

--- js/main.js
+++ js/main.js
@@ -2,10 +2,11 @@ let app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
-        image: "./assets/vmSocks-blue-onWhite.jpg",
+        image: "./assets/vmSocks-green-onWhite.jpg",
         altText: "A pair of socks",
-        link:"https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
-        
-     }     
+        inStock: true,
+        inventory: 100,
+        onSale: true,
+    }
  })
  
