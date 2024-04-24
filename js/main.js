459bca510ca272abf7606eef3aed3a27e869ddef
spacecreator8 <spcc888@ya.ru>
2024-04-24 13:42:25 +0700

    8.Вычисляемые свойства

--- js/main.js
+++ js/main.js
@@ -2,31 +2,52 @@ let app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
-        image: "./assets/vmSocks-green-onWhite.jpg",
+        brand: 'Vue Mastery',  
+        selectedVariant: 0,       
+        
         altText: "A pair of socks",
-        inStock: false,
+       
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
             {
                 variantId: 2234,
                 variantColor: 'green',
                 variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                variantQuantity: 10,
+                onSale:true,
             },
             {
                 variantId: 2235,
                 variantColor: 'blue',
                 variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                variantQuantity: 0,
+                onSale:false,
             }
-        ],
+         ],
         cart: 0
     },
     methods: {
         addToCart() {
             this.cart += 1
         },
-        updateProduct(variantImage) {
-            this.image = variantImage
-        }
-    }
+        updateProduct(index) {
+            this.selectedVariant = index;
+            console.log(index);
+        }         
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product;
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].variantQuantity;
+        },
+        sale(){
+            return this.variants[this.selectedVariant].onSale;
+        }        
+    },
  })
  
